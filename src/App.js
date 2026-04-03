efef377a024eda9a2c556aabe6ff10c54f295c9d
ayusshdixit <samratdixit05@gmail.com>
2026-04-04 00:42:50 +0530

    this is the first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Chai from "./chai";  
+
+function App() {
+  return (
+   <>
+
+      
+      <Chai />
+
+  
+   </>
+
+ 
+  );
+}
+
+export default App;
---
+++ src/chai.js
@@ -0,0 +1,7 @@
+function Chai (){
+    return(
+        <h2>This is Chai Aur Code</h2>
+    )
+}
+
+export  default Chai;
